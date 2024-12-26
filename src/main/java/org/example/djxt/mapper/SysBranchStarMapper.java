9ba7f5f000c19686baa1d69cc624a4615a3a8ffe
however <however-yir@users.noreply.github.com>
2024-12-26 19:05:00 +0800

    refactor: replace wildcard select in branch star mapper

--- src/main/java/org/example/djxt/mapper/SysBranchStarMapper.java
+++ src/main/java/org/example/djxt/mapper/SysBranchStarMapper.java
@@ -13,10 +13,15 @@ import java.util.List;
 @Repository
 public interface SysBranchStarMapper extends BaseMapper<sysBranchStar> {
 
+    String BASE_COLUMNS = "id,sodsacs,partybranchname,establishtime,partynumber,dept_id,dept_name," +
+            "branchuser_id,branchuser_name,branch_secretary_id,branch_secretary_name,self_star,context," +
+            "foundationitem,pluses,minuses,score,starrating,outcome,del_flag,organizationcomment,comments," +
+            "process,sb_year,file_name,file_path,ygly_id,ygly_name,create_by,create_time,update_by,update_time," +
+            "remark,commentitems";
 
-    @Select("select * from sys_branch_star")
+    @Select("select " + BASE_COLUMNS + " from sys_branch_star")
     List<sysBranchStar> selectAll();
 
-    @Select("select * from sys_branch_star where id = #{id}")
+    @Select("select " + BASE_COLUMNS + " from sys_branch_star where id = #{id}")
     sysBranchStar selectById(Long id);
 }
